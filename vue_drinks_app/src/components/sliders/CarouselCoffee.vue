40829396bccedd39df52ee3c02c9ab29b7c09e66
seonnhwa <shwaa826@gmail.com>
2022-12-24 15:37:48 +0900

    feat: fullpage, modal, carousel

---
+++ vue_drinks_app/src/components/sliders/CarouselCoffee.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="carousel w-4/5 h-4/5">
+      <div id="slide1" class="carousel-item relative w-full bg-white">
+        <img src="https://placeimg.com/800/200/arch" class="w-2/5 h-full" />
+        <div class="form-control">
+          <span class="text-xs">메뉴 이름</span>
+          <input type="text" placeholder="Type here" class="input w-full max-w-xs" />
+          <span class="text-xs">매장명</span>
+          <input type="text" placeholder="Type here" class="input w-full max-w-xs" />
+          <span class="text-xs">Keywords</span>
+          <input type="text" placeholder="Type here" class="input w-full max-w-xs" />
+        </div>
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" class="btn btn-circle">❮</a>
+          <a href="#slide2" class="btn btn-circle">❯</a>
+        </div>
+      </div>
+      <div id="slide2" class="carousel-item relative w-full bg-white">
+        <img src="https://placeimg.com/800/200/arch" class="w-2/5 h-full" />
+        <div class="form-control">
+          <span class="text-xs">메뉴 이름</span>
+          <input type="text" placeholder="Type here" class="input w-full max-w-xs" />
+          <span class="text-xs">매장명</span>
+          <input type="text" placeholder="Type here" class="input w-full max-w-xs" />
+          <span class="text-xs">Keywords</span>
+          <input type="text" placeholder="Type here" class="input w-full max-w-xs" />
+        </div>
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" class="btn btn-circle">❮</a>
+          <a href="#slide3" class="btn btn-circle">❯</a>
+        </div>
+      </div>
+      <div id="slide3" class="carousel-item relative w-full bg-white">
+        <img src="https://placeimg.com/800/200/arch" class="w-2/5 h-full" />
+        <div class="form-control">
+          <span class="text-xs">메뉴 이름</span>
+          <input type="text" placeholder="Type here" class="input w-full max-w-xs" />
+          <span class="text-xs">매장명</span>
+          <input type="text" placeholder="Type here" class="input w-full max-w-xs" />
+          <span class="text-xs">Keywords</span>
+          <input type="text" placeholder="Type here" class="input w-full max-w-xs" />
+        </div>
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" class="btn btn-circle">❮</a>
+          <a href="#slide4" class="btn btn-circle">❯</a>
+        </div>
+      </div>
+      <div id="slide4" class="carousel-item relative w-full bg-white">
+        <img src="https://placeimg.com/800/200/arch" class="w-2/5 h-full" />
+        <div class="form-control">
+          <span class="text-xs">메뉴 이름</span>
+          <input type="text" placeholder="Type here" class="input w-full max-w-xs" />
+          <span class="text-xs">매장명</span>
+          <input type="text" placeholder="Type here" class="input w-full max-w-xs" />
+          <span class="text-xs">Keywords</span>
+          <input type="text" placeholder="Type here" class="input w-full max-w-xs" />
+        </div>
+        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" class="btn btn-circle">❮</a>
+          <a href="#slide1" class="btn btn-circle">❯</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- vue_drinks_app/src/views/HomeView.vue
+++ vue_drinks_app/src/views/HomeView.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="snap-y snap-mandatory h-screen w-screen overflow-scroll scrollbar-hide">
+      <app-header v-if="$route.meta.header !== false" class="fixed" />
+      <div class="snap-start bg-amber-50 h-screen flex items-center justify-center text-8xl">
+        <modal-btn />
+        <carousel-coffee />
+      </div>
+      <div class="snap-start bg-sky-100 h-screen flex items-center justify-center text-8xl">
+        <carousel-coffee />
+      </div>
+      <div class="snap-start bg-violet-50 h-screen flex items-center justify-center text-8xl">
+        <carousel-coffee />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Header from '../components/layout/Header'
+import CarouselCoffee from '../components/sliders/CarouselCoffee'
+import Modal from '../components/modal/ModalComponent.vue'
 
 export default {
-  name: "HomeView",
   components: {
-    HelloWorld,
-  },
-};
+    'app-header': Header,
+    'carousel-coffee': CarouselCoffee,
+    'modal-btn': Modal
+  }
+}
 </script>
